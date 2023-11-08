d358baa686e9b36accd2be58c365fcab4f583cbd
Manuree <wen_manuree@protonmail.com>
2023-11-08 09:36:43 +0700

    Adding Dropdown Texture size

--- __init__.py
+++ __init__.py
@@ -7,9 +7,24 @@ import os
 
 plugin_widgets = []
 output_path = ""
+texture_sizes_index = 2
+texture_sizes = ["128x128", "256x256", "512x512", "1024x1024", "2048x2048", "4096x4096"]
+def textureSize(size):
+    if size == "128x128":
+        return 7
+    elif size == "256x256":
+        return 8
+    elif size == "512x512":
+        return 9
+    elif size == "1024x1024":
+        return 10
+    elif size == "2048x2048":
+        return 11
+    elif size == "4096x4096":
+        return 12
 
-
-def export_enfution(output_path, maps):
+def export_enfution(output_path, maps, selected_size):
+    saveData(output_path)
     if not substance_painter.project.is_open():
         return
 
@@ -30,6 +45,7 @@ def export_enfution(output_path, maps):
         Path = os.path.dirname(Path) + "/"
 
     #project_output_directory = substance_painter.project.get_output_path()
+
     config = {
         "exportShaderParams": False,
         "exportPath": Path,
@@ -41,7 +57,7 @@ def export_enfution(output_path, maps):
         "defaultExportPreset": export_preset.url(),
         "exportParameters": [
             {
-                "parameters": {"paddingAlgorithm": "infinite"}
+                "parameters": {"paddingAlgorithm": "infinite", "sizeLog2":textureSize(selected_size)}
             }
         ]
     }
@@ -54,7 +70,7 @@ def export_enfution(output_path, maps):
 def logX():
     for shelf in substance_painter.resource.Shelves.all():
         export_presets_dir = f"{shelf.path()}/export-presets"
-        print(shelf)
+        #print(shelf)
         if not os.path.isdir(export_presets_dir):
             continue
         for filename in os.listdir(export_presets_dir):
@@ -63,7 +79,7 @@ def logX():
             name = os.path.splitext(filename)[0]
             export_preset_id = substance_painter.resource.ResourceID(context=shelf.name(), name=name)
             export_preset = substance_painter.resource.Resource.retrieve(export_preset_id)[0]
-            print(export_preset.gui_name())
+            #print(export_preset.gui_name())
     # my_shelf = substance_painter.resource.Shelf("export") 
     # all_shelf_resources = my_shelf.resources() 
     # print(substance_painter)
@@ -71,21 +87,30 @@ def logX():
     #     print(resource.identifier().name) 
     #print("The name of the project is now: '{0}'".format(substance_painter.project.name())) 
     metadata = substance_painter.project.Metadata("PluginSaveData")
-    metadata.set("plugin_save_data", "testsavedata")
+    save = ["testsavedata", 2]
+    metadata.set("plugin_save_data", save)
+    
+    print("Save Data", metadata.get("plugin_save_data"))
     
 
 def saveData(path):
+    global size_dropdown
     metadata = substance_painter.project.Metadata("PluginSaveData")
-    metadata.set("plugin_save_data", path)
+    PluginSaveData = [path, size_dropdown.currentIndex()]
+    metadata.set("plugin_save_data", PluginSaveData)
     print("Save Data", metadata.get("plugin_save_data"))
 
 def my_callback(*args, **kwargs):
-    global output_path, output_path_input
+    global output_path, output_path_input, texture_sizes_index
     print(f'Callback: {substance_painter.project.file_path()}')
 
     metadata = substance_painter.project.Metadata("PluginSaveData")
-    output_path = metadata.get("plugin_save_data")
+    PluginSaveData = metadata.get("plugin_save_data")
+    output_path = PluginSaveData[0]
+    texture_sizes_index = PluginSaveData[1]
+    print(PluginSaveData)
     output_path_input.setText(output_path)
+    size_dropdown.setCurrentIndex(texture_sizes_index)
     
 def saveTriggered(*args, **kwargs):
     print("S A V E")
@@ -100,13 +125,19 @@ def start_plugin():
     plugin_widget.setWindowTitle("Hello Export") 
     
     substance_painter.event.DISPATCHER.connect(substance_painter.event.ProjectOpened, my_callback)
-    substance_painter.event.DISPATCHER.connect(substance_painter.event.ProjectSaved, saveTriggered)
-
+    #substance_painter.event.DISPATCHER.connect(substance_painter.event.ProjectSaved, saveTriggered)
+    # Create a dropdown for selecting the texture size
+    global size_dropdown
+    size_label = QtWidgets.QLabel("Texture Size:")
+    size_dropdown = QtWidgets.QComboBox()
+    size_dropdown.addItems(texture_sizes)
+    
+    #size_dropdown.activated.conext(saveData(size_dropdown.currentIndex()))
+    
     global output_path_input
     output_path_input = QtWidgets.QLineEdit()
     output_path_input.setPlaceholderText("Output Path")
     output_path_input.setText(output_path)
-       
     output_path_input.textChanged.connect(lambda text = output_path_input.text(): saveData(text))
     
 
@@ -118,15 +149,17 @@ def start_plugin():
     bt_logX = QtWidgets.QPushButton("LogX")
 
     # Connect the button to the export_textures function with the output path as an argument
-    bt_export_mask.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_GLOBAL_MASK"))
-    bt_export_vfx.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_VFX"))
-    bt_export_mcr.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_MCR"))
-    bt_export_nmo.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_NMO"))
+    bt_export_mask.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_GLOBAL_MASK", size_dropdown.currentText()))
+    bt_export_vfx.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_VFX", size_dropdown.currentText()))
+    bt_export_mcr.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_MCR", size_dropdown.currentText()))
+    bt_export_nmo.clicked.connect(lambda: export_enfution(output_path_input.text(), "$textureSet_NMO", size_dropdown.currentText()))
     bt_logX.clicked.connect(lambda: logX())
 
     # Add the label, input field, and button to the layout
     # layout.addWidget(label)
     layout.addWidget(output_path_input)
+    layout.addWidget(size_label)
+    layout.addWidget(size_dropdown)
     layout.addWidget(bt_export_mask)
     layout.addWidget(bt_export_vfx)
     layout.addWidget(bt_export_mcr)
